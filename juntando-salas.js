92fa334f3192b78ef81785394e778a116a93456e
dudu <eduardo.gomes@gafit.com.br>
2023-01-17 09:07:16 -0300

    Aula 2 de JS

---
+++ juntando-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ["Dudu", "Vini", "Henrique"];
+const salaPython = ["Dani", "Josy", "Rafa"];
+
+const salasUni = salaJS.concat(salaPython);
+
+console.log(salasUni);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,10 @@
+const aluno = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8 , 7.5, 9];
+
+const listaDeAlunosEMedias = [aluno, medias];
+
+console.log(
+    `A aluna na posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}.
+    A nota dessa aluna é: ${listaDeAlunosEMedias[1][1]}
+    `
+    );
